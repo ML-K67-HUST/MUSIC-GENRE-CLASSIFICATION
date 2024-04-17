2fe5960cf96906600bb1bf2bcf50675ff0546cf0
ktuanPT373 <phmtrntuankhang@gmail.com>
2024-04-17 18:29:35 +0700

    update visualization

--- Machine Learning /make_prediction_3s.py
+++ Machine Learning /make_prediction_3s.py
@@ -15,11 +15,7 @@ hop_length = 512
 n_fft = 2048
 
 # Function to extract features from audio file
-def extract_features(audio_file):
-    # Load audio file
-    y, sr = librosa.load(audio_file)
-    l = len(y)//2
-    y = y[l-15*sr:l+15*sr]
+def extract_features(y, sr):
     # Extract features
     chroma_stft_mean = librosa.feature.chroma_stft(y=y, sr=sr, hop_length=hop_length).mean()
     chroma_stft_var = librosa.feature.chroma_stft(y=y, sr=sr, hop_length=hop_length).var()
@@ -69,12 +65,59 @@ def extract_features(audio_file):
     for i in range(1, 21):
         features[f'mfcc{i}_mean'] = [mfcc_means[i-1]]
         features[f'mfcc{i}_var'] = [mfcc_vars[i-1]]
-    
     return features
-def predict(aud):
-    features_sample = extract_features(aud)
-    return (KNN_predict(features_sample))
+def analyze_audio(audio_file):
+    y, sr = librosa.load(audio_file)
+    l = len(y)//2
+    features_comb = []
+    start = 0
+    while start + 3*sr < len(y):
+        features_comb.append((extract_features(y[start:start+3*sr],sr),start))
+        start = start + 3*sr
+    return features_comb
+# def predict(aud):
+#     dic = {}
+#     features_comb = analyze_audio(aud)
+#     for c,r in features_comb:
+#         pred = KNN_predict(c)
+#         dic[pred] = dic.get(pred,0)+1
+#     return dic
+import matplotlib.pyplot as plt
+import numpy as np
 
-print(predict("/home/khangpt/MUSIC-GEN-PROJ/User Data Test Songs/classiccal_test.mp3"))
+# Define genre colors
+genre_colors = {
+    "blues": "blue",
+    "classical": "green",
+    "country": "red",
+    "disco": "cyan",
+    "hiphop": "magenta",
+    "jazz": "yellow",
+    "metal": "orange",
+    "pop": "purple",
+    "reggae": "brown",
+    "rock": "gray"
+}
+genre_labels = list(genre_colors.keys())
+
+# Function to predict and visualize the distribution of genres
+def predict_and_visualize(aud):
+    features_comb = analyze_audio(aud)
+    for (c, r) in features_comb:
+        pred = KNN_predict(c)
+        genre_color = genre_colors.get(pred, "black")
+        start_time = r   # Start time of the segment in seconds
+        plt.scatter(start_time, 0, color=genre_color, marker='o', s=100)
+    
+    # Plot settings
+    handles = [plt.scatter([], [], color=color, label=genre) for genre, color in genre_colors.items()]
+    plt.legend(handles, genre_labels, loc='upper right')
+
+    plt.show()
+
+# Call the function to predict and visualize
+print('Prediction result for classical_test.mp3:')
+predict_and_visualize("/home/khangpt/MUSIC-GEN-PROJ/User Data Test Songs/Yeu5-Rhymastic.mp3")
+
 
 
