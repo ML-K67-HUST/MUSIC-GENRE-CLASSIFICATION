07135e259b58447f3376b5ccb0b42560bdb5bdec
ktuanPT373 <phmtrntuankhang@gmail.com>
2024-04-24 09:33:29 +0700

    update svm

--- Machine Learning /support_vector_machine.py
+++ Machine Learning /support_vector_machine.py
@@ -20,28 +20,15 @@ for col in df.iloc[:,:-1]:
 df = df.drop(columns=non_floats)
 
 L = len(df.columns)
-# All the features are put into a matrix
 X = df.iloc[:,:L-1].values
-# Genre tags are converted to categorical data
 df.label = pd.Categorical(df.label)
-# Each genre is encoded as a numrical code
 y = np.array(df.label.cat.codes)
 
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
 
 scaler = StandardScaler()
-# X_train = scaler.fit_transform(X_train)
-# X_test = scaler.transform(X_test)
 
 
-# # Create an SVM object with a rbf kernel
 clf = svm.SVC(kernel='rbf', gamma='scale', C=50)
-
-# # Fit the model to the training data (X_train contains the features and y_train contains the genre labels)
-# clf.fit(X_train, y_train)
-
-# # Use the trained model to predict the genre labels of the test data
-# y_pred = clf.predict(X_test)
 
 clf.fit(X,y)
 
