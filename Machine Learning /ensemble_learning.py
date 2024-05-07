0486d02204d02f57d1f797fcc0f744b90bb51c4a
ktuanPT373 <phmtrntuankhang@gmail.com>
2024-05-07 23:07:52 +0700

    anh khang cook điên

--- Machine Learning /ensemble_learning.py
+++ Machine Learning /ensemble_learning.py
@@ -1,20 +1,26 @@
 import pandas as pd
+import numpy as np
 import os 
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
 from lightgbm import LGBMClassifier
 from sklearn.pipeline import make_pipeline
 import pickle 
 
 
-data = pd.read_csv("GTZAN/Data/features_3_sec.csv")
-df = data.sample(frac=1).reset_index(drop=True)
+df = pd.read_csv('/home/khangpt/MUSIC-GEN-PROJ/GTZAN/Data/features_30_sec.csv')
+df = df.iloc[0:,1:]
+X = df.drop(['length','label'], axis = 1)
+y = df['label']
 
-x = df.drop(['filename', 'length',"label"], axis=1)
-y = df["label"]
+df.label = pd.Categorical(df.label)
+y = np.array(df.label.cat.codes)
+scaler = MinMaxScaler()
+X = scaler.fit_transform(X)
 
-model = make_pipeline(StandardScaler(),
-                        LGBMClassifier(boosting_type= 'gbdt', learning_rate=0.2, max_depth= 7, num_leaves=150))
-model.fit(x, y)
+model = LGBMClassifier(boosting_type= 'gbdt', learning_rate=0.2, max_depth= 7, num_leaves=150)
+model.fit(X, y)
 
-with open('ens_model.pkl','wb') as file:
-    pickle.dump(model,file)
+if os.path.exists('saved_model/ens_model.pkl'):
+    os.remove('saved_model/ens_model.pkl')
+with open('saved_model/ens_model.pkl','wb') as file:
+    pickle.dump(model, file)
--- Machine Learning /k_nearest_neighbor.py
+++ Machine Learning /k_nearest_neighbor.py
@@ -1,19 +1,26 @@
 
 import pandas as pd
 import pickle
-from sklearn.preprocessing import StandardScaler
+import os
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.pipeline import make_pipeline
 import numpy as np
 
 df = pd.read_csv('/home/khangpt/MUSIC-GEN-PROJ/GTZAN/Data/features_30_sec.csv')
 df = df.iloc[0:,1:]
-x = df.drop(['length','label'], axis = 1)
+X = df.drop(['length','label'], axis = 1)
 y = df['label']
+
 df.label = pd.Categorical(df.label)
 y = np.array(df.label.cat.codes)
-KNN = make_pipeline(StandardScaler(), 
-                    KNeighborsClassifier(n_neighbors=1))
-KNN.fit(x, y)
-with open('knn_model.pkl','wb') as file:
-  pickle.dump(KNN, file)
+scaler = MinMaxScaler()
+X = scaler.fit_transform(X)
+
+KNN = KNeighborsClassifier(n_neighbors=5)
+KNN.fit(X, y)
+if os.path.exists('saved_model/knn_model.pkl'):
+    os.remove('saved_model/knn_model.pkl')
+with open('saved_model/knn_model.pkl','wb') as file:
+    pickle.dump(KNN, file)
+
