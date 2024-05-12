b106c828187e024f6b9ee8192f20fe2faee75fbb
ktuanPT373 <phmtrntuankhang@gmail.com>
2024-05-12 11:26:52 +0700

    update

--- machine_learning/k_nearest_neighbor.py
+++ machine_learning/k_nearest_neighbor.py
@@ -2,20 +2,13 @@
 import pandas as pd
 import pickle
 import os
-from sklearn.preprocessing import MinMaxScaler
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.pipeline import make_pipeline
+from preprocess import preprocess_
 import numpy as np
 
 df = pd.read_csv('/home/khangpt/MUSIC-GEN-PROJ/GTZAN/Data/features_30_sec.csv')
-df = df.iloc[0:,1:]
-X = df.drop(['length','label'], axis = 1)
-y = df['label']
 
-df.label = pd.Categorical(df.label)
-y = np.array(df.label.cat.codes)
-scaler = MinMaxScaler()
-X = scaler.fit_transform(X)
+X, y = preprocess_(df)
 
 KNN = KNeighborsClassifier(n_neighbors=5)
 KNN.fit(X, y)
